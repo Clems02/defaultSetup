fc406bb30d96b43c50bfa588e8d653c546a76429
Clems02 <chaumont.clement@outlook.fr>
2024-04-27 17:30:12 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import Theme from "./theme/Theme";
+
+const App = () => {
+  return (
+    <Theme>
+      <Typography>Base pour create project</Typography>
+    </Theme>
+  );
+};
+
+export default App;
